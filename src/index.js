9cb28fa9e52bf0ae862a83c2bd3d97d62198e16f
Forest Monsen <forest.monsen@gmail.com>
2017-11-25 13:11:29 -0800

    Adding Axios to do the XHR

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 class App extends Component {
   constructor() {
     super()
+  }
+  getUsers() {
+    axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
+    .then((res) => { console.log(res); })
+    .catch((err) => { console.log(err); })
   }
   render() {
     return (
