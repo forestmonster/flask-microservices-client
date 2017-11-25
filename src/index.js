4f59f507008a61d1f31a8ef5c59c2b25cef17ff5
Forest Monsen <forest.monsen@gmail.com>
2017-11-25 12:47:33 -0800

    Change to a class-based React component

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col-md-4">
-          <br />
-          <h1>All Users</h1>
-          <hr /><br />
+class App extends Component {
+  constructor() {
+    super()
+  }
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <br />
+            <h1>All Users</h1>
+            <hr /><br />
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 ReactDOM.render(
